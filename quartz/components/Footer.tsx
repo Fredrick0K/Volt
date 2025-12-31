b285f5ea91906488371e7ed635dc3a853eda7dc2
Fredrick0K <moretoxo@gmail.com>
2025-12-31 12:01:21 +0100

    Quartz sync: Dec 31, 2025, 12:01 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -3,15 +3,11 @@ import style from "./styles/footer.scss"
 
 interface Options {
   links: Record<string, string>
-  githubRepo?: string
 }
 
 export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const links = opts?.links ?? []
-    const visitorBadgeUrl = opts?.githubRepo 
-      ? `https://visitor-badge.laobi.icu/?username=${opts.githubRepo.split('/')[0]}&repo=${opts.githubRepo.split('/')[1]}&label=visitors&style=flat&color=blue&textColor=white`
-      : null
 
     return (
       <footer class={`${displayClass ?? ""}`}>
@@ -22,16 +18,6 @@ export default ((opts?: Options) => {
             </li>
           ))}
         </ul>
-        {visitorBadgeUrl && (
-          <div class="visitor-counter">
-            <img 
-              src={visitorBadgeUrl} 
-              alt="visitor count"
-              loading="lazy"
-              style="max-height: 20px;"
-            />
-          </div>
-        )}
       </footer>
     )
   }
